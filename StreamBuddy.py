21a141bc0de92957b8cb35747b6ebbb5215d2bf5
Ian McGahren <fullstackeidolon@gmail.com>
2023-10-23 15:15:40 -0400

    Finalized StreamBuddy for release v1.0
    Fixed up banner name parsing, bug handling, logging, and set banners to end early via the play_video function

--- StreamBuddy.py
+++ StreamBuddy.py
@@ -2,18 +2,78 @@ import os
 import vlc
 import time
 import argparse
-
+import xml.etree.ElementTree as ET
 import logging
+import tkinter as tk
+from tkinter import filedialog
+import urllib.parse
+
+#Set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
+
+# Function to select a folder
+def select_folder(dialog_title="Select a folder"):
+    root = tk.Tk()  # Create a root window
+    root.withdraw()  # Hide the root window
+
+    folder_selected = filedialog.askdirectory(title=dialog_title)  # Open folder selection dialog
+    return folder_selected
+# Select a file function
+def select_file(dialog_title="Select a file", filetypes=None):
+    """
+    Open a file selection dialog.
+
+    Args:
+        dialog_title (str): Title for the file selection dialog.
+        filetypes (list): List of tuples specifying the file types to filter. Each tuple contains a description and a file extension pattern.
+
+    Returns:
+        str: Path of the selected file.
+    """
+    root = tk.Tk()  # Create a root window
+    root.withdraw()  # Hide the root window
+
+    if filetypes is None:
+        filetypes = [("All files", "*.*")]
+
+    file_selected = filedialog.askopenfilename(title=dialog_title, filetypes=filetypes)
+    return file_selected
+
+
+
 #Assemble your video list
+# Video list from folder
 def get_video_files(folder_path):
     video_extensions = ['.mp4', '.avi', '.mkv', '.flv', '.mov']
     files = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if os.path.splitext(f)[1] in video_extensions]
     return sorted(files)
 
+#Video list from .xspf
+def queueBuilder(file_path):
+    # Parse the XML file
+    tree = ET.parse(file_path)
+    root = tree.getroot()
+
+    # Namespace for XSPF
+    ns = {'xspf': 'http://xspf.org/ns/0/'}
+    
+    # List to hold the queue
+    queue = []
+
+    # Iterate through each track in the playlist
+    for track in root.findall('xspf:trackList/xspf:track/xspf:location', ns):
+        location = track.text
+        # Remove the "file://" prefix from the location
+        location_cleaned = location.replace("file:///", "")
+        # Decode the URL-encoded path
+        location_decoded = urllib.parse.unquote(location_cleaned)
+        queue.append(location_decoded)
+
+    return queue
+
 #Handles playing the current video (banner or episode)
-def play_video(video_path, test_mode=False):
+def play_video(video_path, test_mode=False, banner_folder_path=None):
     logger.info(f"Attempting to play video: {video_path}")
     
     # Initialize VLC instance
@@ -21,7 +81,7 @@ def play_video(video_path, test_mode=False):
         vlc_instance = vlc.Instance("--no-xlib --quiet")
         logger.info("VLC Test instance initialized.")
     else:
-        vlc_instance = vlc.Instance("--no-xlib --quiet --fullscreen --video-on-top")
+        vlc_instance = vlc.Instance("--no-xlib --quiet --fullscreen")
         logger.info("VLC instance initialized.")
     
     # Create a new media player object
@@ -35,23 +95,40 @@ def play_video(video_path, test_mode=False):
     # Play the video
     player.play()
     logger.info("Play command sent to VLC player.")
+    
+    # Wait for the video to start playing
+    while not player.is_playing():
+        logger.debug("Waiting for video to start playing...")
+        time.sleep(1)
+    
+    # Try to get the duration until a valid duration is fetched
+    duration_milliseconds = 0
+    while duration_milliseconds <= 0:
+        duration_milliseconds = player.get_length()
+        time.sleep(0.5)  # Slight delay before rechecking
+    
+    duration_seconds = duration_milliseconds / 1000
+    logger.info(f"Video duration: {duration_seconds} seconds")
+    
     if test_mode:
         player.set_fullscreen(False)
-    else:
-        player.set_fullscreen(True)
-    
-    if test_mode:
         logger.info("Test mode active. Playing video for 5 seconds only.")
-        time.sleep(5)
+        time.sleep(20)
         player.stop()
     else:
-        # Wait for video to start playing
-        while not player.is_playing():
-            logger.debug("Waiting for video to start playing...")
-            time.sleep(1)
-
-        logger.info("Video started playing.")
-    
+        player.set_fullscreen(True)
+        
+        # Sleep until 2 seconds before the video ends (ensure non-negative value)
+        sleep_time = max(0, duration_seconds - 2)
+        time.sleep(sleep_time)
+        
+        # Remove the title text overlay from OBS
+        logger.info('Clearing OBS title text...')
+        set_obs_text_title("", "", "", banner_folder_path)
+        
+        # Sleep for the remaining time or at least 2 seconds
+        time.sleep(min(2, duration_seconds - sleep_time))
+        
         # Wait for video to finish playing
         while player.is_playing():
             logger.debug("Video is playing...")
@@ -60,66 +137,115 @@ def play_video(video_path, test_mode=False):
         logger.info("Video playback completed.")
         player.stop()
 
+
+
 # Extract the episode title from the filename
 def extract_episode_title(episode_name):
-    """Extracts the episode title or returns an empty string if the input is empty."""
+    """Extracts the episode title based on the naming conventions."""
     # If the input string is empty, return an empty string
     if not episode_name:
         return ""
     
+    # Remove the file extension
+    episode_name = episode_name.rsplit('.', 1)[0]
+    
+    # Split the string based on ' - ' delimiter
     segments = episode_name.split(' - ')
     
-    # If there are at least three segments, extract the episode title
-    if len(segments) >= 3:
-        return segments[2].rsplit('.', 1)[0]
+    # Check for the naming convention "Show Name - Episode#Season# - Episode Title"
+    if len(segments) == 3:
+        return f"{segments[0]} - {segments[2]}"
     
-    # Otherwise, return the entire input string
+    # Check for the naming convention "Show Name - Episode Title"
+    elif len(segments) == 2:
+        return f"{segments[0]} - {segments[1]}"
+    
+    # If none of the conventions match, return the entire input string
     return episode_name
 
-def set_obs_text_title(banner_text, banner_file_path):    
+def set_obs_text_title(last_episode_title, current_episode_title, next_episode_title, banner_folder_path):    
+    # Default messages for last and next episodes
+    if last_episode_title is None:
+        last_episode_title = "Welcome to the stream!"
+    if next_episode_title is None:
+        next_episode_title = "Thanks for watching!"
+    
+    logger.info(f"Setting OBS title text to: Last: {last_episode_title}, Current: {current_episode_title},  Next: {next_episode_title}")
+    
+    # Define the file paths for the three text files
+    last_episode_file = os.path.join(banner_folder_path, "last_episode.txt")
+    current_episode_file = os.path.join(banner_folder_path, "current_episode.txt")
+    next_episode_file = os.path.join(banner_folder_path, "next_episode.txt")
 
-    try:
-        # Update the bannertext.txt file with the extracted episode title
-        with open(banner_file_path, 'w') as file:
-            file.write(banner_text)
-        logger.info(f"Updated bannertext.txt with: {banner_text}")
-    except Exception as e:
-        logger.error(f"Error updating bannertext.txt: {e}")
+    # Helper function to write to a file and log the action
+    def write_and_log(file_path, content):
+        try:
+            with open(file_path, 'w') as file:
+                file.write(content)
+            logger.info(f"Updated {file_path} with: {content}")
+        except Exception as e:
+            logger.error(f"Error updating {file_path}: {e}")
+
+    # Update the three text files
+    write_and_log(last_episode_file, last_episode_title)
+    write_and_log(current_episode_file, current_episode_title)
+    write_and_log(next_episode_file, next_episode_title)
 
 #Main function
 def main(test_mode=False):
 
-    # SET ENV VARIABLES HERE
+    # SET ENV VARIABLES HERE 
+    queue_xspf_location = select_file(dialog_title="Select an XSPF playlist", filetypes=[("XSPF files", "*.xspf"), ("All files", "*.*")])
+    logger.info('Set up a folder with three text files for the banner text. The text files should be named lastEpisode.txt, currentEpisode.txt, and nextEpisode.txt.')
+    banner_folder_path = select_folder("Select a folder with banner text files")
+    bumper_video_folder_path = select_folder("Select a folder with bumper videos")
+    #banner_video = select_file(dialog_title="Select a banner video", filetypes=[("MP4 files", "*.mp4"), ("All files", "*.*")])
     
-    #folder_path = input("Enter the path to the video folder: ")
-    folder_path = r"D:\TV\Over the Garden Wall (2014)\Season 1"
-    #banner_video = input("Enter the path to the banner video: ")
-    banner_video = r"C:\Users\Eidolon\Videos\banner\banner.mp4"
-    # banner_file_path = input("Enter the path to the banner text file: ")
-    banner_file_path = r"C:\Users\Eidolon\Videos\banner\bannertext.txt"
+    #Set up indexes for looping
+    current_episode_index = -1
+    bumper_index = 0
+    logger.info('Loading video and bumper queue...')
     
-    logger.info('Loading video queue from folder...')
-    video_queue = get_video_files(folder_path)
-
+    #video_queue = queueBuilder(folder_path)
+    #Q: how do I use queueBuilder instead of queueBuilder?
+    video_queue = queueBuilder(queue_xspf_location)
+    bumper_queue = get_video_files(bumper_video_folder_path)
     try:
         while True:
-            if not video_queue:
+            if not video_queue or current_episode_index == len(video_queue) - 1:
                 logger.info('Reloading video queue from folder...')
-                video_queue = get_video_files(folder_path)
+                video_queue = queueBuilder(queue_xspf_location)
+                current_episode_index = 0
+
+            # Get the last, current, and next episode titles
+            last_episode = video_queue[current_episode_index - 1] if current_episode_index > 0 else None
+            current_episode = video_queue[current_episode_index]
+            next_episode = video_queue[current_episode_index + 1] if current_episode_index < len(video_queue) - 1 else None
+
+            # Extract titles
+            last_episode_title = extract_episode_title(os.path.basename(last_episode)) if last_episode else None
+            current_episode_title = extract_episode_title(os.path.basename(current_episode))
+            next_episode_title = extract_episode_title(os.path.basename(next_episode)) if next_episode else None
 
             # Set the title text of the next episode on OBS
-            next_episode_title = os.path.basename(video_queue[0])
-            banner_text = extract_episode_title(next_episode_title)
-            logger.info(f'Setting OBS title text to: {banner_text}')
-            set_obs_text_title(banner_text, banner_file_path)
+            #logger.info(f'Setting OBS title text to: Last: {last_episode_title}, Current: {current_episode_title},  Next: {next_episode_title}')
+            set_obs_text_title(last_episode_title, current_episode_title, next_episode_title, banner_folder_path)
+        
+            # Increment the index for next iteration (simulate playing the current episode)
+            current_episode_index += 1
 
-            # Play banner video
-            logger.info('Playing banner video...')
-            play_video(banner_video, test_mode=test_mode)
+            # Play bumper video
+            current_bumper_video = bumper_queue[bumper_index]
+            logger.info(f'Playing bumper video: {current_bumper_video}')
+            play_video(current_bumper_video, test_mode=test_mode, banner_folder_path=banner_folder_path)
+
 
             # Remove the title text overlay from OBS
             logger.info('Clearing OBS title text...')
-            set_obs_text_title("", banner_file_path)
+            set_obs_text_title("", "", "", banner_folder_path)
+
+            # Increment bumper index or reset if we've reached the end of bumper_queue
+            bumper_index = (bumper_index + 1) % len(bumper_queue)
 
             # Play the next episode
             next_episode = video_queue.pop(0)
